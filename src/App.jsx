54bec9b681d67af50306e72a650ca6037d471db2
John Golloshi <johngolloshi@gmail.com>
2023-09-05 15:28:49 +0100

    fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import './App.css'
+import { createClient } from "contentful"
+import {useState, useEffect} from "react";
 
-function App() {
+function App() { 
 // John
+const [recipes, setRecipes] = useState([]);
+const client = createClient({
+  space:"z6djbtsjwacx",
+  accessToken:"btwYTx3RnBmekwMDx69C54Rk2IiPLtq9qzG8e3e7p1s"
+})
+useEffect(() => {
+  async function getRecipes() {
+  const entryItems = await client.getEntries();
+  console.log(entryItems.items);
+  setRecipes(entryItems.items);
+}
+  getRecipes();
+},[]);
 
 //Vaishali
 
@@ -9,11 +24,11 @@ function App() {
 
   return (
     <>
-      {/* john */}
+    {/* john */}
+      <h1>{}</h1>
+    {/* vaishali */}
 
-      {/* vaishali */}
-
-      {/* sonia */}
+    {/* sonia */}
       
     </>
   )
